f94fba7f520ee0f0f63bfee1138209dee10e9a54
Caleb-sb <52993870+Caleb-sb@users.noreply.github.com>
2019-09-30 11:55:14 +0200

    Added initial header file

---
+++ miniProject.h
@@ -0,0 +1,31 @@
+/*
+ * miniProject.cpp
+ *
+ * Written for EEE3096S 2019 by Caleb Bredekamp
+ */
+
+#ifndef MINIPROJECT_H
+#define MINIPROJECT_H
+
+//Includes
+#include <wiringPi.h>
+#include <vector> //lists was used for buffer?
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+#include <math.h>
+#include <pthread.h>
+#include <iostream>
+
+//Define buttons
+#define _BTN
+#define _BTN
+#define _BTN
+#define _BTN
+
+//SPI Settings
+#define SPI_CHAN // Write your value here
+#define SPI_SPEED // Write your value here
+
+#endif
