f63f5001a49e5c34c5d7f360a8b39e884cff00df
Sama <sama.naraghi@gmail.com>
2019-10-11 16:58:41 +0200

    light works

--- src/miniProject.c
+++ src/miniProject.c
@@ -5,9 +5,11 @@ using namespace std;
 
 unsigned char startByte = 1;
 unsigned char sendByte;
-char configByte = 0b10000000;
+char configByte0 = 0b10000000;
+char configByte1 = 0b10010000;
 
-unsigned short adcVal;
+unsigned short humidityVal, lightVal;
+double Vout, Vhum, Vlig;
 
 unsigned char buffer[3];
 
@@ -35,18 +37,30 @@ int main(void){
     }
     while (true){
         buffer[0] = startByte;
-        buffer[1] = configByte;
+        buffer[1] = configByte0;
         buffer[2] = 0;
 
         wiringPiSPIDataRW (SPI_CHAN, buffer, 3);
         buffer[1] = buffer[1] & 0b00000011;
-        adcVal = (buffer[1] << 8) + buffer[2];
+        humidityVal = (buffer[1] << 8) + buffer[2];
+        Vhum = ((double)humidityVal/(double)1023)*3.3;
+        
+        buffer[0] = startByte;
+        buffer[1] = configByte1;
+        buffer[2] = 0;
+        
+        wiringPiSPIDataRW (SPI_CHAN, buffer, 3);
+        buffer[1] = buffer[1] & 0b00000011;
+        lightVal = (buffer[1] << 8) + buffer[2];
+        Vlig = (double)lightVal/(double)1023;
+        
+        Vout = Vlig * Vhum;
         
         hours = getHours();
         mins = getMins();
         secs = getSecs();
         
-        printf("time: %d:%d:%d      val: %d \n", hours,mins,secs, adcVal);
+        printf("time: %d:%d:%d    humidity: %d light: %d  Vout: %.2f \n", hours,mins,secs, humidityVal, lightVal, Vout);
         
         sleep(1);
     }
